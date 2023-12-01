e447dccc7dbd35d225c577ce280d96f3f9406ca9
Gurukrupa-Export <129185339+Gurukrupa-Export@users.noreply.github.com>
2023-12-01 16:20:08 +0530

    update - 1-12-23
    update

---
+++ custom_export/custom_export/doctype/logistic_invoice/logistic_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, gurukrupa_export] and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Logistic Invoice', {
+	// refresh: function(frm) {
+
+	// }
+});
